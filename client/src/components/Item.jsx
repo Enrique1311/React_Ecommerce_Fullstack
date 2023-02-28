641d24569510eb1f6503fed0ce38e40a99b1f0e4
Enrique1311 <ejspinelli131176@gmail.com>
2023-02-28 17:03:42 -0300

    The Shopping List was added

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { IconButton, Box, Typography, useTheme, Button } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import DeleteForever from '@mui/icons-material/DeleteForever';
 import { shades } from '../theme';
 import { addToCart } from '../state';
 import { useNavigate } from 'react-router-dom';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteForever from '@mui/icons-material/DeleteForever';
 
 const Item = ({ item, width }) => {
 	const navigate = useNavigate();
@@ -16,11 +15,14 @@ const Item = ({ item, width }) => {
 	const {
 		palette: { neutral },
 	} = useTheme();
-	const { category, price, name, image } = item.attibutes;
+
+	const { category, price, name, image } = item.attributes;
 	const {
 		data: {
-			formats: {
-				medium: { url },
+			attributes: {
+				formats: {
+					medium: { url },
+				},
 			},
 		},
 	} = image;
@@ -35,20 +37,19 @@ const Item = ({ item, width }) => {
 				<img
 					alt={item.name}
 					width='300px'
-					height='auto'
-					src={`http://localhost:1337${url}`}
+					height='400px'
+					src={`http://localhost:2000${url}`}
 					onClick={() => navigate(`/item/${item.id}`)}
-					style={{ cursos: 'pointer' }}
+					style={{ cursor: 'pointer' }}
 				/>
 				<Box
-					display={isHovered ? 'blocked' : 'none'}
-					position='relative'
+					display={isHovered ? 'block' : 'none'}
+					position='absolute'
 					bottom='10%'
 					left='0'
 					width='100%'
 					padding='0 5%'
 				>
-					{/* Amount */}
 					<Box
 						display='flex'
 						justifyContent='space-between'
@@ -57,9 +58,8 @@ const Item = ({ item, width }) => {
 							display='flex'
 							alignItems='center'
 							backgroundColor={shades.neutral[100]}
-							borderRadius='5px'
+							borderRadius='3px'
 						>
-							{' '}
 							<IconButton onClick={() => setCount(Math.max(count - 1, 1))}>
 								<DeleteForever />
 							</IconButton>
@@ -68,7 +68,6 @@ const Item = ({ item, width }) => {
 								<AddIcon />
 							</IconButton>
 						</Box>
-						{/* Button */}
 						<Button
 							onClick={() => {
 								dispatch(addToCart({ item: { ...item, count } }));
@@ -80,13 +79,14 @@ const Item = ({ item, width }) => {
 					</Box>
 				</Box>
 			</Box>
+
 			<Box mt='3px'>
 				<Typography
 					variant='subtitle2'
 					color={neutral.dark}
 				>
 					{category
-						.replace(/([A-Z]/g, ' $1')
+						.replace(/([A-Z])/g, ' $1')
 						.replace(/^./, (str) => str.toUpperCase())}
 				</Typography>
 				<Typography>{name}</Typography>
